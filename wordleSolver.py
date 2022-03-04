abb86df08d13dbf97962a1b5e0b0a03ce4d02514
Name <61922260+CreepyMemes@users.noreply.github.com>
2022-03-04 15:41:00 +0100

    updated iswrongpos()
    now also removes words that have yellow characters in those wrong positions

--- wordleSolver.py
+++ wordleSolver.py
@@ -1,6 +1,7 @@
 import re
 
-def welcome(): #main welcome screen
+# Main welcome screen
+def welcome():
     print("----------------------------------------------------------------------------")
     print("                           Welcome to WordleSolver")
     print("----------------------------------------------------------------------------")
@@ -10,48 +11,54 @@ def welcome(): #main welcome screen
     print("      - (lowercase) if letter is in  a  WRONG position   (example: a)")
     print("      - (.letter)   if letter is NOT PRESENT in the word (example: .a)\n")
 
-def readWords(): #read all words in words.txt
+# Reads all words in words.txt
+def readWords():
     with open("words.txt", "r") as inp: return inp.read().split("\n")
 
-def removechars(s, remove): #remove characters in (string remove) from (string s)
-    return ''.join([ char         if char not in remove else ''  for char in s])
+# Removes characters in (string remove) from (string s)
+def removeChars(s, remove):
+    return ''.join([ char         if char not in remove else ''  for char in s ])
 
-def getpos(s, remove, guess): #return a string long 5 with letters in right position (example: f___y_ul)
-    return ''.join([ char.lower() if char in     s      else '_' for char in removechars(guess, remove)])
+# Returns a string long 5 with letters in right position (example: f___y_u)
+def getPos(s, remove, guess):
+    return ''.join([ char.lower() if char     in s      else '_' for char in removeChars(guess, remove) ])
 
-def validate(guess): #decode the input given returning right/wrong position letters and the ones not in word
+# Decodes the input given returning right/wrong position letters and the ones not in word
+def validate(guess):
     notPrese = ''.join([ char[1:] for char in re.findall(r'[.]+\w', guess) ])
-    wrongPos = removechars( re.sub('[^a-z]', '', guess), notPrese )
-    rightPos = getpos     ( re.sub('[^A-Z]', '', guess), notPrese, guess)
+    wrongPos = getPos( removeChars( re.sub('[^a-z]', '', guess), notPrese ), notPrese, guess )
+    rightPos = getPos(              re.sub('[^A-Z]', '', guess),             notPrese, guess )
 
-    notPrese = ''.join([ a for a in notPrese if all( (a != b) for b in rightPos) ]) #remove duplicates from notPrese that are also present in rightPos
-
+    # Removes duplicates from notPrese that are also present in rightPos
+    notPrese = ''.join([ a for a in notPrese if all( (a != b) for b in rightPos) ])
     return (rightPos, wrongPos, notPrese)
 
-def isrightpos(words, right): #remove words that don't have certain characters in right position
-    return [word for word in words if all( (a == b) or (b == "_")  for a, b in zip(word, right) )]
+# Removes words that don't have certain characters in right position
+def isrightpos(words, right):
+    return [word for word in words if all( (a == b)    or (b == "_") for a, b in zip(word, right) )]
 
-def iswrongpos(words, wrong): #remove words that don't have certain characters
-    return [word for word in words if all( char in word  for char in wrong)]
+# Removes words that don't have certain characters AND removes words that have certain characters in the wrong posions
+def iswrongpos(words, wrong): 
+    return [word for word in words if all( (c in word) or (c == "_") for c in wrong ) and all( (a != b) or (b =="_" ) for a,b in zip(word, wrong)) ]
 
-def isnotprese(words, notPrese): # remove words that have characters that aren't present
+# Removes words that have characters that aren't present
+def isnotprese(words, notPrese):
     return [word for word in words if all( (a != b)  for a in word for b in notPrese )]
 
-def update(words, guessdata): #update words list with guess input
+# Update words list with guess input
+def update(words, guessdata):
     words = isnotprese(words, guessdata[2]) 
     words = iswrongpos(words, guessdata[1]) 
     words = isrightpos(words, guessdata[0]) 
-
     return words
 
+# Main program loop
 def main():
+    welcome()
     words = readWords()
-    welcome()   
-
     while True:
         guess = input("\nguess: ")
         words = update(words, validate(guess))
-
         print("\nPossible Words:", words)
 
 if __name__ == '__main__':
